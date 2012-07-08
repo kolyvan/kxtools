ac0145730c9f09820385cdd89c1c45f5248365c6
Kolyvan <ru.kolyvan@gmail.com>
2012-07-09 00:09:51 +1100

    removed an obsolete code from NSData+Kolyvan

--- kxtools/NSData+Kolyvan.m
+++ kxtools/NSData+Kolyvan.m
@@ -177,9 +177,7 @@ static NSData * base64decode (NSData * from)
     return mdata;
 }
 
-
 @implementation NSData (Kolyvan)
-
 
 + (NSData *) dataFromBase64String:(NSString *)string 
 {
@@ -195,91 +193,6 @@ static NSData * base64decode (NSData * from)
                                         encoding:NSASCIIStringEncoding];
     return KX_AUTORELEASE(s);    
 }
-
-/*
-- (NSData *) gzip
-{
-	if ([self length] == 0) {
-        return self;
-    }
-    
-    z_stream zStream;
-    
-    zStream.zalloc = Z_NULL;
-    zStream.zfree = Z_NULL;
-    zStream.opaque = Z_NULL;
-    zStream.next_in = (Bytef *)[self bytes];
-    zStream.avail_in = [self length];
-    zStream.total_out = 0;
-    
-    if (deflateInit(&zStream, Z_DEFAULT_COMPRESSION) != Z_OK) {
-        return nil;
-    }
-    
-    NSUInteger compressionChunkSize = 16384; // 16Kb
-    NSMutableData *compressedData = [NSMutableData dataWithLength:compressionChunkSize];
-    
-    do {
-        if (zStream.total_out >= [compressedData length]) {
-            [compressedData increaseLengthBy:compressionChunkSize];
-        }
-        
-        zStream.next_out = [compressedData mutableBytes] + zStream.total_out;
-        zStream.avail_out = [compressedData length] - zStream.total_out;
-        
-        deflate(&zStream, Z_FINISH);
-        
-    } while (zStream.avail_out == 0);
-    
-    deflateEnd(&zStream);
-    [compressedData setLength:zStream.total_out];
-    
-    return [NSData dataWithData:compressedData];
-}
-
-- (NSData *) gunzip:(NSError **)error 
-{
-    z_stream zStream;
-    
-    zStream.zalloc = Z_NULL;
-    zStream.zfree = Z_NULL;
-    zStream.next_in = (Bytef *)[self bytes];
-    zStream.avail_in = [self length];
-    zStream.avail_out = 0;
-    zStream.total_out = 0;
-    
-    NSUInteger estimatedLength = [self length] / 2;
-    NSMutableData *decompressedData = [NSMutableData dataWithLength:estimatedLength];
-    
-    do {
-        if (zStream.total_out >= [decompressedData length]) {
-            [decompressedData increaseLengthBy:estimatedLength / 2];
-        }
-        
-        zStream.next_out = [decompressedData mutableBytes] + zStream.total_out;
-        zStream.avail_out = [decompressedData length] - zStream.total_out;
-        
-        int status = inflate(&zStream, Z_FINISH);
-        
-        if (status == Z_STREAM_END) {
-            break;
-        } else if (status != Z_OK) {
-            if (error) {
-                *error = [NSError errorWithDomain:@"zlib" code:status userInfo:nil];
-            }
-            
-            return nil;
-        }
-    } while (zStream.avail_out == 0);
-    
-    [decompressedData setLength:zStream.total_out];
-    
-    return [NSData dataWithData:decompressedData];
-}
-
-
-
-*/
 
 - (NSData *) gzip 
 {
